876d511d42b2d7bab4ea2f6f65fff1cd6cce1eac
vignesh boppana <vignesh6boppana@gmail.com>
2023-06-26 12:13:55 -0500

    minor ui changes

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -7,8 +7,15 @@ import { Component } from '@angular/core';
 })
 export class WelcomeComponent {
 
-  modules=[{'text':'Transcribe & Summarize','path':'transcribe'},
-  {'text':'QA Bot',path:'qa'},{'text':'GPT Completion',path:'gpt'},{'text':'Image Generation',path:'imagegen'},{'text':'Image to Image Generation',path:'img2img'},
-  {'text':'AI Upscaler',path:'aiupscaler'},{'text':'Image Classifier',path:'classify'},{'text':'YOLO Classifier',path:'yolo'},
-  {'text':'Video Generation',path:'videogen'}];
+  modules=[
+  {'text':'Transcribe & Summarize','path':'transcribe','description':'Paste youtube url to get transcript & summary.'},
+  {'text':'QA Bot',path:'qa','description':'Given proper context, it can answer questions from the context.'},
+  {'text':'GPT Completion',path:'gpt','description':'given a text, gpt can predict next few sentences'},
+  {'text':'Image Generation',path:'imagegen','description':'given a description, image gen can generate an image'},
+  {'text':'Image to Image Generation',path:'img2img','description':'given a description and an image, image2image can generate a new image'},
+  {'text':'AI Upscaler',path:'aiupscaler','description':'given a image, image gen can upscale the image'},
+  {'text':'Image Classifier',path:'classify','description':'given a image, will classify the largest object in the image'},
+  {'text':'YOLO Classifier',path:'yolo','description':'given a image, yolo can identify all object, animal, people in the image'},
+  {'text':'Video Generation',path:'videogen','description':'given a description, video gen can generate an video'}
+];
 }

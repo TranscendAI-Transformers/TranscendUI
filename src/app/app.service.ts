d7a52e6d3130af6fa987b5a09b4586b2e636390e
vignesh boppana <vignesh6boppana@gmail.com>
2023-06-28 10:46:36 -0500

    added base url

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -5,40 +5,40 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AppService {
-
+  base:string='http://localhost:5000/';
   constructor(private http:HttpClient) { }
 
   runPipeline(data:any){
-    return this.http.post('http://localhost:5000/',data)
+    return this.http.post(this.base,data)
   }
   generateImage(data:any){
-    return this.http.post('http://localhost:5000/imagegen',data,{responseType:'text'})
+    return this.http.post(this.base+'imagegen',data,{responseType:'text'})
   }
   img2img(data:any){
-    return this.http.post('http://localhost:5000/img2img',data,{responseType:'text'})
+    return this.http.post(this.base+'img2img',data,{responseType:'text'})
   }
   upscaleImage(data:any){
-    return this.http.post('http://localhost:5000/aiupscaler',data,{responseType:'text'})
+    return this.http.post(this.base+'aiupscaler',data,{responseType:'text'})
   }
   classifyImage(data:any){
-    return this.http.post('http://localhost:5000/classify',data,{responseType:'text'})
+    return this.http.post(this.base+'classify',data,{responseType:'text'})
   }
   imageCaption(data:any){
-    return this.http.post('http://localhost:5000/image_caption',data)
+    return this.http.post(this.base+'image_caption',data)
   }
   yolo(data:any){
-    return this.http.post('http://localhost:5000/yolo',data)
+    return this.http.post(this.base+'yolo',data)
   }
   askBot(data:any){
-    return this.http.post('http://localhost:5000/ask_bot',data)
+    return this.http.post(this.base+'ask_bot',data)
   }
   gpt(data:any){
-    return this.http.post('http://localhost:5000/text_generation',data)
+    return this.http.post(this.base+'text_generation',data)
   }
   summary(data:any){
-    return this.http.post('http://localhost:5000/summary',data)
+    return this.http.post(this.base+'summary',data)
   }
   generateVideo(data:any){
-    return this.http.post('http://localhost:5000/videogen',data,{responseType:'text'})
+    return this.http.post(this.base+'videogen',data,{responseType:'text'})
   }
 }

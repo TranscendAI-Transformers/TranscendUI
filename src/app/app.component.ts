ffb6bcd1a6e5b72e221097d7ecd742ac7d526aad
vignesh boppana <vignesh6boppana@gmail.com>
2023-06-29 15:27:45 -0500

    dynamic navbar using app service module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,17 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit{
   modules:any[]=[];
-  images:any[]=[];
-  text:any[]=[];
-  video:any[]=[];
+  imageServices:any[]=[];
+  textServices:any[]=[];
+  videoServices:any[]=[];
   constructor(private service:AppService){
     this.modules=service.modules;
-    console.log(this.modules);
   }
 
   ngOnInit(): void {
-        this.images=this.modules.filter(x=>x.category=="Image");
-        this.text=this.modules.filter(x=>x.category=="Text");
-        this.video=this.modules.filter(x=>x.category=="Video");
+        this.imageServices=this.modules.filter(x=>x.category=="Image");
+        this.textServices=this.modules.filter(x=>x.category=="Text");
+        this.videoServices=this.modules.filter(x=>x.category=="Video");
+   
   }
 }

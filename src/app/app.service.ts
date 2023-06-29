e6d67a68bbff6eb235e5eae4be96a76f9576ef00
vignesh boppana <vignesh6boppana@gmail.com>
2023-06-29 15:13:21 -0500

    fetching modules from app service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,6 +6,21 @@ import { Injectable } from '@angular/core';
 })
 export class AppService {
   base:string='http://localhost:5000/';
+  modules=[
+    {'text':'Transcribe & Summarize','path':'transcribe','description':'Paste youtube url to get transcript & summary.','category':'Text'},
+    {'text':'QA Bot',path:'qa','description':'Given proper context, it can answer questions from the context.','category':'Text'},
+    {'text':'GPT Completion',path:'gpt','description':'given a text, gpt can predict next few sentences','category':'Text'},
+    {'text':'Image Generation',path:'imagegen','description':'given a description, image gen can generate an image','category':'Image'},
+    {'text':'Image to Image Generation',path:'img2img','description':'given a description and an image, image2image can generate a new image','category':'Image'},
+    {'text':'AI Upscaler',path:'aiupscaler','description':'given a image, image gen can upscale the image','category':'Image'},
+    {'text':'Image Classifier',path:'classify','description':'given a image, will classify the largest object in the image','category':'Image'},
+    {'text':'YOLO Classifier',path:'yolo','description':'given a image, yolo can identify all object, animal, people in the image','category':'Image'},
+    {'text':'Video Generation',path:'videogen','description':'given a description, video gen can generate an video','category':'Video'},
+    {'path':'summary','text':'Summary','description':'Generate summary from any given context','category':'Text'},
+    {'path':'image_caption','text':'Image Caption','description':'Generate Caption/description from image','category':'Image'},
+    {'path':'image_qa','text':'Image QA','description':'Generate Answers from context available within the image.','category':'Image'},
+  ];
+  
   constructor(private http:HttpClient) { }
 
   runPipeline(data:any){
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-welcome',
@@ -6,19 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  modules:any[]=[];
+  constructor(private service:AppService){
+    this.modules=service.modules;
+  }
 
-  modules=[
-  {'text':'Transcribe & Summarize','path':'transcribe','description':'Paste youtube url to get transcript & summary.'},
-  {'text':'QA Bot',path:'qa','description':'Given proper context, it can answer questions from the context.'},
-  {'text':'GPT Completion',path:'gpt','description':'given a text, gpt can predict next few sentences'},
-  {'text':'Image Generation',path:'imagegen','description':'given a description, image gen can generate an image'},
-  {'text':'Image to Image Generation',path:'img2img','description':'given a description and an image, image2image can generate a new image'},
-  {'text':'AI Upscaler',path:'aiupscaler','description':'given a image, image gen can upscale the image'},
-  {'text':'Image Classifier',path:'classify','description':'given a image, will classify the largest object in the image'},
-  {'text':'YOLO Classifier',path:'yolo','description':'given a image, yolo can identify all object, animal, people in the image'},
-  {'text':'Video Generation',path:'videogen','description':'given a description, video gen can generate an video'},
-  {'path':'summary','text':'Summary','description':'Generate summary from any given context'},
-  {'path':'image_caption','text':'Image Caption','description':'Generate Caption/description from image'},
-  {'path':'image_qa','text':'Image QA','description':'Generate Answers from context available within the image.'},
-];
+ 
 }

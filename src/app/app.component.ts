5aad989dc23731dde3ffa8a712ddcd04dffe29e0
vignesh boppana <vignesh6boppana@gmail.com>
2023-06-29 15:21:04 -0500

    dynamically fetching categories from app service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { AppService } from './app.service';
 
 @Component({
@@ -6,6 +6,19 @@ import { AppService } from './app.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit{
+  modules:any[]=[];
+  images:any[]=[];
+  text:any[]=[];
+  video:any[]=[];
+  constructor(private service:AppService){
+    this.modules=service.modules;
+    console.log(this.modules);
+  }
+
+  ngOnInit(): void {
+        this.images=this.modules.filter(x=>x.category=="Image");
+        this.text=this.modules.filter(x=>x.category=="Text");
+        this.video=this.modules.filter(x=>x.category=="Video");
+  }
 }
